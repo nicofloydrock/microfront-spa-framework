07820bf06a3889cffe653242c77fbee5e184b2d8
Nicolas Rivera <nico.acevedo.nr@gmail.com>
2022-02-03 23:45:33 -0300

    Menu Lateral
    Se agrega menu lateral base , para luego manejar desde aca , el menu y el shell content

--- root-config-app/src/App.tsx
+++ root-config-app/src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button, DatePicker } from 'antd';
 
+import './App.css';
+import 'antd/dist/antd.css';
+import './index.css';
+import React from 'react';
+import Nav from './components/nav-menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-
-           <>
-            <Button type="primary">PRESS ME</Button>
-            <DatePicker placeholder="select date" />
-          </>
-        
-      </header>
-    </div>
+    <React.Fragment>
+      <Nav />
+    </React.Fragment>
   );
 }
 
---
+++ root-config-app/src/components/nav-menu.tsx
@@ -0,0 +1,71 @@
+
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import React, { useState } from 'react';
+
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+
+
+function NavMenu() {
+  const [collapsed, setState] = useState(false);
+
+  const  onCollapse = (collapsed:boolean) => {
+    console.log(collapsed);
+    setState(collapsed);
+  };
+
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+              MICROFRONT HAZE
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />}>
+              Option 2
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9" icon={<FileOutlined />}>
+              Files
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>XXX</Breadcrumb.Item>
+              <Breadcrumb.Item>JJJJ</Breadcrumb.Item>
+            </Breadcrumb>
+            
+
+            <div id="single-spa-application:app-productos"></div>
+            <div id="single-spa-application:app-ventas"></div>
+
+
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
+  );
+}
+
+export default NavMenu;
